1fd7690bba87f576a36ce8e330238d8e40c315fb
royal0706 <victoria_royal@harvard.edu>
2019-04-15 23:41:10 -0400

    Updated Required fields

--- resources/views/trip/search.blade.php
+++ resources/views/trip/search.blade.php
@@ -12,15 +12,17 @@
     <img src='/images/traveljar.jpg' alt='Travel Jar'>
 
     <h2>Trip Saving Calculator</h2>
-    <p>Calculate how much you need to save a month to make the trip of your dreams become a reality!</p>
+    <p>Calculate how much you need to save per month to make the trip of your dreams become a reality!</p>
+
+    <div class='required'>* Required fields</div>
 
     <form method='GET' action='/trip/search-process'>
 
-        <label>Destination:</label>
+        <label>Destination: *</label>
         <input type='text' name='destination' id='destination' value='{{ $destination }}'>
         @include('includes.error-field', ['fieldName' => 'destination'])
 
-        <label>Airfare Total:</label>
+        <label>Airfare Total: *</label>
         <input type='number' name='airfare' id='airfare' value='{{ $airfare }}'>
         @include('includes.error-field', ['fieldName' => 'airfare'])
 
@@ -32,7 +34,7 @@
         </select>
         @include('includes.error-field', ['fieldName' => 'airfareCurrency'])
 
-        <label>Hotel Total:</label>
+        <label>Hotel Total: *</label>
         <input type='number' name='hotel' value='{{ $hotel }}'>
         @include('includes.error-field', ['fieldName' => 'hotel'])
 
@@ -44,7 +46,7 @@
         </select>
         @include('includes.error-field', ['fieldName' => 'hotelCurrency'])
 
-        <label>How long do you have to save for your trip? </label>
+        <label>How long do you have to save for your trip? *</label>
         <ul class='radios'>
             <li><label><input type='radio'
                               name='months'
